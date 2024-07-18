5002eae3f5b68f5bda810c01b3e538690387166d
JuanDiegoG <jdiegogarcia0110@gmail.com>
2024-07-18 16:59:39 -0500

    Se agrega responsive a Navbar y cambio metadatos con el icon

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,32 @@
-import React from "react";
-import './Navbar.css'
+import React, { useState } from "react";
+import './Navbar.css';
 
-const Navbar = () =>{
-    return(
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
         <header className="header">
             <a href="/" className="logo">Juan</a>
 
-            <nav className="navbar">
+            <nav className={`navbar ${isOpen ? "open" : ""}`}>
                 <a href="/">Home</a>
                 <a href="/">About</a>
                 <a href="/">Portfolio</a>
                 <a href="/">Services</a>
                 <a href="/">Contact</a>
             </nav>
-        </header>
-    )
-}
 
-export default Navbar
+            <div className="menu-icon" onClick={toggleMenu}>
+                <div className={`bar ${isOpen ? "open" : ""}`}></div>
+                <div className={`bar ${isOpen ? "open" : ""}`}></div>
+                <div className={`bar ${isOpen ? "open" : ""}`}></div>
+            </div>
+        </header>
+    );
+};
+
+export default Navbar;
